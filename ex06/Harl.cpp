a99c9f36002c6d574584116170d3eddd25ba5fcc
kuyu <kuyu@student.codam.nl>
2026-05-18 13:47:58 +0200

    changed the ex06 to switch structure.

--- ex06/Harl.cpp
+++ ex06/Harl.cpp
@@ -27,27 +27,67 @@ void Harl::error( void )
 
 void Harl::complain( std::string level )
 {
-	// void (Harl::*ptr[])( void ) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-	typedef void (Harl::*funcPtr)( void );
-	funcPtr ptr[4] = {
-		&Harl::debug,
-		&Harl::info,
-		&Harl::warning,
-		&Harl::error
-	};
-	std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-
-	for (int i = 0; i < 4; i++)
+	// new implementation
+	int levelIndex = -1;
+	
+	if (level == "DEBUG")
+		levelIndex = 0;
+	else if (level == "INFO")
+		levelIndex = 1;
+	else if (level == "WARNING")
+		levelIndex = 2;
+	else if (level == "ERROR")
+		levelIndex = 3;
+	
+	switch (levelIndex)
 	{
-		if (level == levels[i])
-		{
-			for (int j = i; j < 4; j++)
-			{
-				(this->*ptr[j])();
-				std::cout << std::endl;
-			}
-			return ;
-		}
+		case 0:
+			debug();
+			std::cout << std::endl;
+			// fall through
+		case 1:
+			info();
+			std::cout << std::endl;
+			// fall through
+		case 2:
+			warning();
+			std::cout << std::endl;
+			// fall through
+		case 3:
+			error();
+			std::cout << std::endl;
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	}
-	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+
 }
+
+/*
+
+	// // void (Harl::*ptr[])( void ) = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	// typedef void (Harl::*funcPtr)( void );
+	// funcPtr ptr[4] = {
+	// 	&Harl::debug,
+	// 	&Harl::info,
+	// 	&Harl::warning,
+	// 	&Harl::error
+	// };
+	// std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	// for (int i = 0; i < 4; i++)
+	// {
+	// 	if (level == levels[i])
+	// 	{
+	// 		for (int j = i; j < 4; j++)
+	// 		{
+	// 			(this->*ptr[j])();
+	// 			std::cout << std::endl;
+	// 		}
+	// 		return ;
+	// 	}
+	// }
+	// std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+
+
+*/
